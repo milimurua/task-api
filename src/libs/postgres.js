640be0d2431ed8553392ddef3273af2e65dcc7e9
milimurua <milagrosmurua2004@gmail.com>
2025-03-20 12:34:08 -0300

    fixed pg_admin connection

--- src/libs/postgres.js
+++ src/libs/postgres.js
@@ -16,13 +16,6 @@ const pool = new Pool({
     connectionTimeoutMillis: 2000
 });
 
-
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
-
 export { pool };
 
 
